c5b2ebc0805590785d159d7e042644095bcd81cf
Zaid M. Said <sentul@brainstorm.pro>
2013-12-30 20:01:17 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardianEnlMy::Application.config.secret_token = 'd99bcd98d538d2813431fb2ac414391d88d740313632f0bd3f6af123e028bc7efdfe374e72dfb0b65c35cd96179d0f636ac10a5cc9d2c0b7812fc49317ee19ce'
