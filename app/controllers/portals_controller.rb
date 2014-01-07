b8e549d7f407fd0fcb9ee8e85f0b26d45b242ae8
Zaid M. Said <sentul@brainstorm.pro>
2014-01-07 15:03:58 +0800

    some work related stuff

--- app/controllers/portals_controller.rb
+++ app/controllers/portals_controller.rb
@@ -19,25 +19,25 @@ class PortalsController < ApplicationController
 
   # GET /portals/1
   # GET /portals/1.json
-  def show
-    @portal = Portal.find(params[:id])
+  # def show
+  #   @portal = Portal.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @portal }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @portal }
+  #   end
+  # end
 
-  # GET /portals/new
-  # GET /portals/new.json
-  def new
-    @portal = Portal.new
+  # # GET /portals/new
+  # # GET /portals/new.json
+  # def new
+  #   @portal = Portal.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @portal }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @portal }
+  #   end
+  # end
 
   # GET /portals/1/edit
   def edit
@@ -47,19 +47,19 @@ class PortalsController < ApplicationController
 
   # POST /portals
   # POST /portals.json
-  def create
-    @portal = Portal.new(params[:portal])
+  # def create
+  #   @portal = Portal.new(params[:portal])
 
-    respond_to do |format|
-      if @portal.save
-        format.html { redirect_to portals_url, notice: 'Portal was successfully created.' }
-        format.json { render json: @portal, status: :created, location: @portal }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @portal.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @portal.save
+  #       format.html { redirect_to portals_url, notice: 'Portal was successfully created.' }
+  #       format.json { render json: @portal, status: :created, location: @portal }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @portal.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # GET /portals/seed
   def seed
@@ -106,15 +106,15 @@ class PortalsController < ApplicationController
 
   # DELETE /portals/1
   # DELETE /portals/1.json
-  def destroy
-    @portal = Portal.find(params[:id])
-    @portal.destroy
+  # def destroy
+  #   @portal = Portal.find(params[:id])
+  #   @portal.destroy
 
-    respond_to do |format|
-      format.html { redirect_to portals_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to portals_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
