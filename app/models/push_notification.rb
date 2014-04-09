54f15a9d3576367ff70c5f9ec5a6334a34f641ac
Zaid M. Said <sentul@brainstorm.pro>
2014-04-09 14:38:23 +0800

    disable push

--- app/models/push_notification.rb
+++ app/models/push_notification.rb
@@ -2,18 +2,18 @@ require "net/http"
 require "net/https"
 class PushNotification
   def self.welcome(user)
-    message_options = {
-      :registration_ids => [<array of registration ids>],
-      # optional parameters below.  Read the docs here: http://developer.android.com/guide/google/gcm/gcm.html#send-msg
-      :collapse_key => "foobar",
-      :data => { :score => "3x1" },
-      :delay_while_idle => false,
-      :time_to_live => 1
-    }
+    # message_options = {
+    #   :registration_ids => [<array of registration ids>],
+    #   # optional parameters below.  Read the docs here: http://developer.android.com/guide/google/gcm/gcm.html#send-msg
+    #   :collapse_key => "foobar",
+    #   :data => { :score => "3x1" },
+    #   :delay_while_idle => false,
+    #   :time_to_live => 1
+    # }
 
-    response = SpeedyGCM::API.send_notification(message_options)
+    # response = SpeedyGCM::API.send_notification(message_options)
 
-    puts response[:code]  # some http response code like 200
-    puts response[:data]  # usually nil is returned
+    # puts response[:code]  # some http response code like 200
+    # puts response[:data]  # usually nil is returned
   end
 end
